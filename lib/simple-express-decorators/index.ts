81ee8368c393c91f5a1e790d7de826bc3d43bae2
suhail-ansari-apconic <suhaila@apconic.com>
2018-08-30 00:59:41 +0530

    Exported context for external clients as well

--- lib/simple-express-decorators/index.ts
+++ lib/simple-express-decorators/index.ts
@@ -1,4 +1,5 @@
 import Server from './server';
+import Context from './context';
 import { controller, get, post, del } from './route-decorators';
 
-export { Server, controller, get, post, del };
+export { Server, Context, controller, get, post, del };
